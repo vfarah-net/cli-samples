61f9b552e2ec6dea34ed88c98c5312e5c3b3ca6e
David Fowler <davidfowl@gmail.com>
2016-01-30 16:28:27 -0800

    Works better :)

--- HelloMvc/Startup.cs
+++ HelloMvc/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace HelloMvc
 {
@@ -12,8 +13,10 @@ namespace HelloMvc
             services.AddMvc();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(LogLevel.Debug);
+
             app.UseIISPlatformHandler();
             app.UseForwardedHeaders(new ForwardedHeadersOptions
             {
